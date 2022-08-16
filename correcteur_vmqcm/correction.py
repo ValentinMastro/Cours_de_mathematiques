3eb5c745007867bf8842538b9f3d99c4df8d01c5
Valentin Mastro <valentinmastro@hotmail.fr>
2022-08-16 12:03:13 +0200

    correcteur associé à la classe LaTeX vmqcm

---
+++ correcteur_vmqcm/correction.py
@@ -0,0 +1,25 @@
+"""
+    L'objectif de ce programme est de corriger les interrogations générés avec la classe LaTeX "vmqcm.cls".
+    
+    AVANT D'UTILISER LE PROGRAMME
+    -> Scanner toutes les copies des élèves (dans le même sens) et les réunir en un fichier pdf, 
+        où chaque page correspond à la copie d'un élève.
+    -> Préparer un fichier eleves.csv contenant, pour chaque élève, les informations suivantes :
+        nom, prénom, niveau (6ème, 5ème, etc.), classe (A, B, C), numéro élève
+    
+    DÉROULÉ DU PROGRAMME 
+    (pour chaque copie d'élève)
+        -> Scanner le QR Code, contenant le nombre de questions, les réponses aux questions, et le nombre de points par question
+        -> Scanner les informations élèves
+        -> Scanner les réponses de l'élève
+        -> Calculer la note de l'élève
+    -> Générer un fichier csv avec les informations élèves et leur note
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
