ad3aaf62d5021e19f379c401315ec634f172d121
Valentin Mastro <valentinmastro@hotmail.fr>
2023-02-03 13:44:41 +0100

    maj scan.Sh

--- correcteur_vmqcm/scan.sh
+++ correcteur_vmqcm/scan.sh
@@ -1,4 +1,3 @@
 # On scanne la feuille (148x210 -> format A5)
 EPOCH=`date +%s%N`
-scanimage -d "brother5:bus2;dev2" --format=png --progress --resolution 400 -x 148 -y 210 > "eval9_5B/5B_$EPOCH.png"
-python src/scan_pdf_2.py "eval9_5B/5B_$EPOCH.png" data.csv
+scanimage -d "brother5:bus2;dev2" --format=png --progress --resolution 400 -x 148 -y 210 > "eval12_4E/4E_$EPOCH.png"
